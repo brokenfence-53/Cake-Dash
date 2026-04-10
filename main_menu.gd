1510e54b8d3af7dd3c059bccf8b19979acef9dc0
brokenfence-53 <brokenfence444@gmail.com>
2026-04-09 22:46:26 -0400

    adding buttons to main menu script

--- main_menu.gd
+++ main_menu.gd
@@ -9,3 +9,15 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_continue_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_new_game_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	pass # Replace with function body.
