5d38424b1c5a72c4c4fe0e7abe09e751be7ad87b
brokenfence-53 <brokenfence444@gmail.com>
2026-04-09 22:05:51 -0400

    added to main script and fix to indent
    might create a level folder sort but now the indenting works

---
+++ main.gd
@@ -0,0 +1,11 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	get_tree().change_scene_to_file(str("res://main_menu.tscn"))
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
