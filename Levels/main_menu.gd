768e75eee2f00c8b327f662b93f1ae9b9f9aa842
Pixe1at3dG1itch <pixelgitch3@gmail.com>
2026-04-09 23:18:10 -0700

    menu updated

--- Levels/main_menu.gd
+++ Levels/main_menu.gd
@@ -1,23 +1,22 @@
 extends Control
+@onready var HoverSFX : AudioStreamPlayer = $Audio/HoverSFX
+@onready var SelectSFX : AudioStreamPlayer = $Audio/SelectSFX
+@onready var MenuMusic : AudioStreamPlayer = $Audio/MenuMusic
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	MenuMusic.play()
+
+func _process(_delta: float) -> void:
 	pass
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 
 func _on_continue_pressed() -> void:
-	get_tree().change_scene_to_file(str("res://Levels/main.tscn")) # Replace with function body.
-
+	MenuMusic.stop()
+	SelectSFX.play()
+	get_tree().change_scene_to_file("res://Levels/main.tscn") 
 
 func _on_new_game_pressed() -> void:
-	get_tree().change_scene_to_file(str("res://Levels/main.tscn")) # Replace with function body.
-
+	SelectSFX.play()
+	get_tree().change_scene_to_file("res://Levels/main.tscn")
 
 func _on_quit_pressed() -> void:
-	get_tree().quit() # Replace with function body.
+	get_tree().quit() 
